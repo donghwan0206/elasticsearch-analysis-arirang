e48394ed0f64e212bad954a60323448d28ededc3
donghwan <donghwan0206@gmail.com>
2023-07-03 15:35:41 +0900

    refactoring variable

--- src/main/java/org/elasticsearch/index/analysis/ArirangAnalyzerProvider.java
+++ src/main/java/org/elasticsearch/index/analysis/ArirangAnalyzerProvider.java
@@ -11,31 +11,24 @@ public class ArirangAnalyzerProvider extends AbstractIndexAnalyzerProvider<Korea
 
     private final KoreanAnalyzer analyzer;
 
-    private boolean bigrammable = false;
-    private boolean preserveOrigin = false;
-    private boolean exactMatch = false;
-    private boolean preserveCNoun = true;
-    private boolean decompound = true;
-    private boolean wordSegment = true;
-
     public ArirangAnalyzerProvider(IndexSettings indexSettings, Environment env, String name, Settings settings) throws IOException {
         super(indexSettings, name, settings);
 
-        bigrammable = settings.getAsBoolean("bigrammable", false);
-        preserveOrigin = settings.getAsBoolean("preserveOrigin", false);
-        exactMatch = settings.getAsBoolean("exactMatch", false);
-        preserveCNoun = settings.getAsBoolean("preserveCNoun", true);
-        decompound = settings.getAsBoolean("decompound", true);
-        wordSegment = settings.getAsBoolean("wordSegment", true);
+        boolean bigrammable = settings.getAsBoolean("bigrammable", false);
+        boolean preserveOrigin = settings.getAsBoolean("preserveOrigin", false);
+        boolean exactMatch = settings.getAsBoolean("exactMatch", false);
+        boolean preserveCNoun = settings.getAsBoolean("preserveCNoun", true);
+        boolean decompound = settings.getAsBoolean("decompound", true);
+        boolean wordSegment = settings.getAsBoolean("wordSegment", true);
 
         analyzer = new KoreanAnalyzer();
 
-        analyzer.setBigrammable(this.bigrammable);
-        analyzer.setHasOrigin(this.preserveOrigin);
-        analyzer.setExactMatch(this.exactMatch);
-        analyzer.setOriginCNoun(this.preserveCNoun);
-        analyzer.setDecompound(this.decompound);
-        analyzer.setWordSegment(this.wordSegment);
+        analyzer.setBigrammable(bigrammable);
+        analyzer.setHasOrigin(preserveOrigin);
+        analyzer.setExactMatch(exactMatch);
+        analyzer.setOriginCNoun(preserveCNoun);
+        analyzer.setDecompound(decompound);
+        analyzer.setWordSegment(wordSegment);
 
     }
 
